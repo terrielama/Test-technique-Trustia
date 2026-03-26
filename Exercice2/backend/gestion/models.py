12efb5757ddd2b2b337383beedd7b96cc7b62092
TerrieL <60408333+terrielama@users.noreply.github.com>
2026-03-26 11:15:31 +0100

    Création des 3 principaux modeles
    Création du dossier frontend

--- Exercice2/backend/gestion/models.py
+++ Exercice2/backend/gestion/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+#---------- Modèle Produit ----------
+# id, nom, prix, date de péremption.
+class Produit(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.FloatField()
+    date_peremption = models.DateField()
+
+    def __str__(self):
+        return self.nom
+
+# ---------- Modèle Facture ----------
+# Objectif : calculer le total de la facture et le total des produits
+
+class Facture(models.Model):
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def total_facture(self):
+        total = sum(item.produit.prix * item.quantite for item in self.details.all())
+        return total
+
+    def total_produits(self):
+        return sum(item.quantite for item in self.details.all())
+
+
+#--- Modèle Détail d'une Facture  ---
+# Objectif : représenter les détails d'une facture (produit + quantité)
+
+class DetailFacture(models.Model):
+    facture = models.ForeignKey(Facture, related_name='details', on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
  Exercie 1 : Créer un programme python qui affiche dans la console des blocs de texte encadrés
 
  Exercie 2 : Créer une application web Django permettant de gérer des produits et de générer des factures.
+
+    Lancer le serveur Django :
+    python manage.py runserver
